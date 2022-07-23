e0c62c5499bbdd9fa6b5338237454091addb4ad0
walrusray811 <walrusray811@gmail.com>
2022-07-24 03:31:57 +0900

    add responsive css for mobile

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,6 +1,7 @@
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import { EmailImage, GitHubLogoImage } from "./Images";
+import * as styles from "./ProfileCard.module.css";
 
 interface ProfileCardProps {
   name: string;
@@ -15,23 +16,23 @@ const ProfileCard = ({
   email,
   githubUrl,
 }: ProfileCardProps) => {
-  const IMAGE_SIZE = 160;
+  const SM_IMAGE_SIZE = 160;
   const IMAGE_PATH = "../images/profile.png";
   const ICON_SIZE = 28;
   return (
-    <figure className="flex gap-8 p-2 w-full items-center">
+    <figure className="flex flex-col sm:flex-row gap-2 sm:gap-8 p-2 w-full items-center">
       <StaticImage
-        className="rounded-full flex"
+        className={`rounded-full flex ${styles.ProfileCardImage}`}
         src={IMAGE_PATH}
         alt="profile"
         aspectRatio={1}
-        width={IMAGE_SIZE}
+        width={SM_IMAGE_SIZE}
         layout="fixed"
         placeholder="dominantColor"
       ></StaticImage>
-      <figcaption className="flex flex-col gap-4 truncate">
+      <figcaption className="flex sm:flex-col gap-2 sm:gap-4 truncate ">
         <h1 className="text-3xl font-bold">{name}</h1>
-        <p className="break-all">{description}</p>
+        <p className="break-all hidden sm:block">{description}</p>
         <a
           href={`mailto:${email}`}
           target="_blank"
@@ -43,7 +44,7 @@ const ProfileCard = ({
             height={ICON_SIZE}
             fill="#000000"
           ></EmailImage>
-          <span className="group-hover:text-violet-700 dark:group-hover:text-peach-500 group-hover:underline underline-offset-2">
+          <span className="group-hover:text-violet-700 dark:group-hover:text-peach-500 group-hover:underline underline-offset-2 hidden sm:block">
             {email}
           </span>
         </a>
@@ -58,7 +59,7 @@ const ProfileCard = ({
             height={ICON_SIZE}
             fill="#000000"
           ></GitHubLogoImage>
-          <span className="group-hover:text-violet-700 dark:group-hover:text-peach-500 group-hover:underline underline-offset-2">
+          <span className="group-hover:text-violet-700 dark:group-hover:text-peach-500 group-hover:underline underline-offset-2 hidden sm:block">
             {githubUrl}
           </span>
         </a>
