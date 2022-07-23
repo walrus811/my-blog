70a509f2a9b99988b1a543a6edf954384bf006af
walrusray811 <walrusray811@gmail.com>
2022-07-24 02:45:37 +0900

    add site metadata and exclue some editor preferences

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,14 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: "Log"
+    title: "We can work it out",
+    description: "My Blog",
+    profile: {
+      name: "Ray",
+      description: "너무 졸리다. 그냥 잘까. 고민 됩니다. 화이팅.",
+      email: "walrusray811@gmail.com",
+      githubUrl: "https://github.com/walrus811"
+    }
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
